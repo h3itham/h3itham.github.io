df36d840a0514eb4194e0e826a9ba28be014d7b9
haitham <haithame.shabaan@vodafone.com>
2025-09-18 14:29:39 +0300

    new them

--- js/main.js
+++ js/main.js
@@ -30,13 +30,16 @@
 
     // Typed Initiate
     if ($('.typed-text-output').length == 1) {
-        var typed_strings = $('.typed-text').text();
         var typed = new Typed('.typed-text-output', {
-            strings: typed_strings.split(', '),
+            strings: ['DevOps Engineer', 'Cloud Engineer', 'Linux Administrator'],
             typeSpeed: 100,
-            backSpeed: 20,
-            smartBackspace: false,
-            loop: true
+            backSpeed: 50,
+            backDelay: 1000,
+            startDelay: 500,
+            smartBackspace: true,
+            loop: true,
+            showCursor: true,
+            cursorChar: '|'
         });
     }
 
